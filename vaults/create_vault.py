e58f78266afa3de3db2274595819651542bebf07
kwilt <42013501+kwilt@users.noreply.github.com>
2024-08-05 13:25:26 -0500

    Update create_vault.py

--- vaults/create_vault.py
+++ vaults/create_vault.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 filename = input("Enter name of new password file: ")
 password = getpass("Enter the decryption password: ")
-data_input = getpass("Enter the password to be hidden: ")
+data_input = getpass("Enter the password to be encrypted: ")
 
 data = bytes(data_input, encoding="utf-8")
 filepath = Path(__file__).with_name(filename) # output file in same directory as the script
