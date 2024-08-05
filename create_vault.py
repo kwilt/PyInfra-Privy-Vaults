f264b52ba54727cf01a16cc7edd1554c2b62fd8a
kwilt <42013501+kwilt@users.noreply.github.com>
2024-08-05 12:41:58 -0500

    Create create_vault.py

---
+++ create_vault.py
@@ -0,0 +1,13 @@
+import privy
+from getpass import getpass
+from pathlib import Path
+
+filename = input("Enter name of new password file: ")
+password = getpass('Enter the decryption password: ')
+data_input = getpass("Enter the password to be hidden: ")
+
+data = bytes(data_input, encoding="utf-8")
+filepath = Path(__file__).with_name(filename) # output file in same directory as the script
+
+with open(filepath, "w+b") as encrypted_data:
+    encrypted_data.write(privy.hide(data, password).encode())
